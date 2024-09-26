0973541dd01a168bd0b66142b3812b0ae27b5bdb
Jintao Yu <1007936662@qq.com>
2024-09-26 20:43:04 +0200

    add configuration file

--- apps/Interactive-Volumetric-Renderer/main.cpp
+++ apps/Interactive-Volumetric-Renderer/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <test.hpp>
+#include <util/configuration.hpp>
 
 int main(int, char**){
     func();
---
+++ include/util/configuration.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+/**
+ * @enum SCENES
+ * @brief Describe the various scenes of showcase we have
+ * 
+ * IntSys: Interactive System
+ * VolFog: Volumetric Fog
+ */
+enum SCENES {
+    IntSys,
+    VolFog,
+};
